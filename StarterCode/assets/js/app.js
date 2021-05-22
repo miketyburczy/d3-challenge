31b40dd143c825e4e00d63259b94fe22845e8494
Mike Tyburczy <miketyburczy@mikes-mbp.lan>
2021-05-22 14:04:15 -0400

    Commit

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,15 @@
 // @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var vvgHeight = 620;
+
+var margin = {
+    top:20,
+    right: 40,
+    bottom: 200,
+    left: 100
+};
+
+var width = svgWidth - margin.right - margin.left;
+
+var height = svgHeight - margin.top - margin.bottom;
+
