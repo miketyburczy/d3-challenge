a34e374bff5b9ff9a6a6d56d939f960c5ca3e3fb
Mike Tyburczy <miketyburczy@mikes-mbp.lan>
2021-05-22 14:22:20 -0400

    Progress

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -13,3 +13,17 @@ var width = svgWidth - margin.right - margin.left;
 
 var height = svgHeight - margin.top - margin.bottom;
 
+var svg = svg = d3.select("scatter")
+    .append("svg")
+    .attr("width", "svgWidth")
+    .attr("height", "svgHeight");
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.csv("assets/data/data.csv").then(function (stateData) {
+    console.log(stateData);
+    stateData.forEach(function (data) {
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
+    })})
